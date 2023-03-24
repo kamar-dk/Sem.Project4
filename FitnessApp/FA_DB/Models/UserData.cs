e8d7cfbaa695edd3309bf7edde481ba61bc0b467
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-24 12:29:43 +0100

    Create UserData.cs

---
+++ FitnessApp/FA_DB/Models/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FA_DB.Models
+{
+    [PrimaryKey(nameof(Email))]
+    public class UserData
+    {
+        [Key]
+        public string Email { get; set; }
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public string Gender { get; set; }
+        public DateTime DoB { get; set; }
+
+        public User user { get; set; }
+    }
+}
