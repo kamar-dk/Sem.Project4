6551a96a34959ccf70bb8bf197d36a3471dddf70
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-24 13:03:38 +0100

    Create RunningSession.cs

---
+++ FitnessApp/FA_DB/Models/TraningTypes/RunningSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FA_DB.Models.TraningTypes
+{
+    public class RunningSession
+    {
+        [Key]
+        public int SessionId { get; set; }
+        public DateTime Date { get; set; }
+        public TimeOnly Durration { get; set; }
+        public float Distance { get; set; }
+        public float AvgSpeed { get; set; }
+        public string Note { get; set; }
+
+        public TraningData traningData { get; set; }
+    }
+}
