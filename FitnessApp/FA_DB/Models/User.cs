8aa6af609e59b9a911f5df83d2f748be23ebb3f7
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-24 12:03:58 +0100

    Create User.cs

---
+++ FitnessApp/FA_DB/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FA_DB.Models
+{
+    public class User
+    {
+        [Key]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
