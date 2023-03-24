7510d80f48e84c90af7728f71a771bbbd2c76a7c
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-24 13:14:48 +0100

    Update DataContext.cs

--- FitnessApp/FA_DB/Data/DataContext.cs
+++ FitnessApp/FA_DB/Data/DataContext.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FA_DB.Models;
+
 
 namespace FA_DB.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DbSet<User> users { get; set; } = default;
+        public DbSet<UserData> userDatas { get; set; } = default;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            
+        }
+
     }
 }
