b2c682d61cfec25ca0abf2351f4948b5cbe08137
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-24 13:03:41 +0100

    Create TraningData.cs

---
+++ FitnessApp/FA_DB/Models/TraningData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FA_DB.Models.TraningTypes;
+
+namespace FA_DB.Models
+{
+    public class TraningData
+    {
+        [Key]
+        public string Email { get; set; }
+        public List<RunningSession> RunningSessions { get; set; } = null;
+    }
+}
