6a01c70d71175950d2a7c2a2db7c335fd5430d82
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-24 13:14:53 +0100

    Update User.cs

--- FitnessApp/FA_DB/Models/User.cs
+++ FitnessApp/FA_DB/Models/User.cs
@@ -5,12 +5,12 @@ namespace FA_DB.Models
 {
     public class User
     {
-        //[Key]
+        [Key]
         public string Email { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public UserData UserData { get; set; }
+        public UserData UserData { get; set; } 
     }
 }
