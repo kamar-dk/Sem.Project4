0c96ad3fc050d5adfb6af5ffad3f2c2dcb3f21da
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-25 12:22:09 +0100

    Update RunningSession.cs

--- FitnessApp/FA_DB/Models/TraningTypes/RunningSession.cs
+++ FitnessApp/FA_DB/Models/TraningTypes/RunningSession.cs
@@ -13,7 +13,7 @@ namespace FA_DB.Models.TraningTypes
         [Key]
         public int SessionId { get; set; }
         public DateTime Date { get; set; }
-        public TimeOnly Durration { get; set; }
+        public float Durration { get; set; }
         public float Distance { get; set; }
         public float AvgSpeed { get; set; }
         public string Note { get; set; }
