7af439f15da10af4766d59e77a0fff2bad0f64b8
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-25 12:22:02 +0100

    Update UserData.cs

--- FitnessApp/FA_DB/Models/UserData.cs
+++ FitnessApp/FA_DB/Models/UserData.cs
@@ -9,16 +9,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FA_DB.Models
 {
-    [PrimaryKey(nameof(Email))]
+    
     public class UserData
     {
-        [Key]
         public string Email { get; set; }
         public float Weight { get; set; }
         public float Height { get; set; }
         public string Gender { get; set; }
         public DateTime DoB { get; set; }
-
-        public User user { get; set; }
+        public User User { get; set; }
     }
 }
