29acfd54856e753299b749a0d079fde56f7816a2
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-30 15:24:19 +0200

    Create TraningProgram.cs

---
+++ FitnessApp/FA_DB/Models/TraningProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA_DB.Models
+{
+    public class TraningProgram
+    {
+        public int TraningProgramID { get; set; }
+        public string Name { get; set; }
+    }
+}
