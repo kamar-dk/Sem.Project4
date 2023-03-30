21106bf1eb141abdacfa6083f5db124503213670
TigrisDK <45198976+TigrisDK@users.noreply.github.com>
2023-03-30 15:39:22 +0200

    Update DataContext.cs

--- FitnessApp/FA_DB/Data/DataContext.cs
+++ FitnessApp/FA_DB/Data/DataContext.cs
@@ -10,6 +10,9 @@ namespace FA_DB.Data
         public DbSet<User> users { get; set; }
         public DbSet<UserData> userDatas { get; set; }
         public DbSet<TraningData> trantingData { get; set; }
+        public DbSet<FavoriteTraningPrograms> favoriteTraningPrograms { get; set; }
+        public DbSet<TraningProgram> traningPrograms { get; set; }
+
         
         // TraningSessions
         public DbSet<RunningSession> runningSessions { get; set; }
@@ -21,16 +24,34 @@ namespace FA_DB.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Define keys
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Email);
+            modelBuilder.Entity<UserData>()
+                .HasKey(ud => ud.Email);
+            modelBuilder.Entity<TraningData>()
+                .HasKey(td => td.Email);
+            modelBuilder.Entity<FavoriteTraningPrograms>()
+                .HasKey(ftp => ftp.Email);
+            modelBuilder.Entity<TraningProgram>()
+                .HasKey(tp => tp.TraningProgramID);
+            modelBuilder.Entity<RunningSession>()
+                .HasKey(rs => rs.SessionID);
+
+
+            // Define User Relationships
             modelBuilder.Entity<User>()
                 .HasOne(u => u.TraningData)
                 .WithOne(td => td.User)
                 .HasForeignKey<TraningData>(td => td.Email);
-
             modelBuilder.Entity<User>()
                 .HasOne(u => u.UserData)
                 .WithOne(ud => ud.User)
                 .HasForeignKey<UserData>(ud => ud.Email);
-            
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.FavoriteTraningPrograms)
+                .WithOne(ftp => ftp.User)
+                .HasForeignKey<FavoriteTraningPrograms>(ftp => ftp.Email);
         }
 
     }
