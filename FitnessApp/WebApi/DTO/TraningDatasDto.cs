81c1957f9a5d913dd28fc0d59ef2e149b4df2bdc
AlanKhamo <alankhamo@gmail.com>
2023-04-28 14:33:17 +0200

    .

---
+++ FitnessApp/WebApi/DTO/TraningDatasDto.cs
@@ -0,0 +1,10 @@
+﻿using FA_DB.Models;
+
+namespace WebApi.DTO
+{
+    public class TraningDatasDto
+    {
+       public string Email { get; set; }
+        //public User User { get; set; }
+    }
+}
---
+++ FitnessApp/WebApi/DTO/UserDatasDto.cs
@@ -0,0 +1,15 @@
+﻿using FA_DB.Models;
+
+namespace WebApi.DTO
+{
+    public class UserDatasDto
+    {
+        public string Email { get; set; }
+        //public User User { get; set; }
+
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public string Gender { get; set; }
+        public DateTime DoB { get; set; }
+    }
+}
