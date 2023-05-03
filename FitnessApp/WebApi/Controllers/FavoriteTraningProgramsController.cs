05665c8e1bd13e8a686ab06ef7986ad41dbbc3be
Bateminho <93334175+Bateminho@users.noreply.github.com>
2023-05-03 13:09:52 +0200

    UserweightSController modified

--- FitnessApp/WebApi/Controllers/FavoriteTraningProgramsController.cs
+++ FitnessApp/WebApi/Controllers/FavoriteTraningProgramsController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FA_DB.Data;
-using FA_DB.Models;
 
 namespace WebApi.Controllers
 {
@@ -25,118 +22,11 @@ namespace WebApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<string>>> GetfavoriteTraningPrograms()
         {
-          if (_context.favoriteTraningPrograms == null)
-          {
-              return NotFound();
-          }
+            var programNames = _context.favoriteTraningPrograms
+                .SelectMany(f => f.TraningPrograms)
+                .Select(p => p.Name);
 
-          var programNames = _context.favoriteTraningPrograms.SelectMany(f => f.TraningPrograms).Select(p => p.Name);
-          
-          return await programNames.ToListAsync();
-          //return await _context.favoriteTraningPrograms.ToListAsync();
+            return await programNames.ToListAsync();
         }
-
-        // GET: api/FavoriteTraningPrograms/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<FavoriteTraningPrograms>> GetFavoriteTraningPrograms(string id)
-        {
-          if (_context.favoriteTraningPrograms == null)
-          {
-              return NotFound();
-          }
-            var favoriteTraningPrograms = await _context.favoriteTraningPrograms.FindAsync(id);
-
-            if (favoriteTraningPrograms == null)
-            {
-                return NotFound();
-            }
-
-            return favoriteTraningPrograms;
-        }
-
-        //    // PUT: api/FavoriteTraningPrograms/5
-        //    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //    [HttpPut("{id}")]
-        //    public async Task<IActionResult> PutFavoriteTraningPrograms(string id, FavoriteTraningPrograms favoriteTraningPrograms)
-        //    {
-        //        if (id != favoriteTraningPrograms.Email)
-        //        {
-        //            return BadRequest();
-        //        }
-
-        //        _context.Entry(favoriteTraningPrograms).State = EntityState.Modified;
-
-        //        try
-        //        {
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!FavoriteTraningProgramsExists(id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-
-        //        return NoContent();
-        //    }
-
-        //    // POST: api/FavoriteTraningPrograms
-        //    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //    [HttpPost]
-        //    public async Task<ActionResult<FavoriteTraningPrograms>> PostFavoriteTraningPrograms(FavoriteTraningPrograms favoriteTraningPrograms)
-        //    {
-        //      if (_context.favoriteTraningPrograms == null)
-        //      {
-        //          return Problem("Entity set 'DataContext.favoriteTraningPrograms'  is null.");
-        //      }
-        //        _context.favoriteTraningPrograms.Add(favoriteTraningPrograms);
-        //        try
-        //        {
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateException)
-        //        {
-        //            if (FavoriteTraningProgramsExists(favoriteTraningPrograms.Email))
-        //            {
-        //                return Conflict();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-
-        //        return CreatedAtAction("GetFavoriteTraningPrograms", new { id = favoriteTraningPrograms.Email }, favoriteTraningPrograms);
-        //    }
-
-        //    // DELETE: api/FavoriteTraningPrograms/5
-        //    [HttpDelete("{id}")]
-        //    public async Task<IActionResult> DeleteFavoriteTraningPrograms(string id)
-        //    {
-        //        if (_context.favoriteTraningPrograms == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        var favoriteTraningPrograms = await _context.favoriteTraningPrograms.FindAsync(id);
-        //        if (favoriteTraningPrograms == null)
-        //        {
-        //            return NotFound();
-        //        }
-
-        //        _context.favoriteTraningPrograms.Remove(favoriteTraningPrograms);
-        //        await _context.SaveChangesAsync();
-
-        //        return NoContent();
-        //    }
-
-        //    private bool FavoriteTraningProgramsExists(string id)
-        //    {
-        //        return (_context.favoriteTraningPrograms?.Any(e => e.Email == id)).GetValueOrDefault();
-        //    }
     }
 }
--- FitnessApp/WebApi/Controllers/UserWeightController.cs
+++ FitnessApp/WebApi/Controllers/UserWeightController.cs
@@ -1,41 +1,37 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using AutoMapper;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using FA_DB.Data;
-//using WebApi.DTO;
-////using WebApi.Models;
-//using FA_DB.Models;
-//using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FA_DB.Data;
+using FA_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-//namespace WebApi.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class UserWeightController : ControllerBase
-//    {
-//        private readonly DataContext _context;
-//        private readonly IMapper _mapper;
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserWeightController : ControllerBase
+    {
+        private readonly DataContext _context;
 
-//        public UserWeightController(DataContext context, IMapper mapper)
-//        {
-//            _context = context;
-//            _mapper = mapper;
-//        }
-//    }
-//    [HttpGet]
-//    public async Task<ActionResult<IEnumerable<UserWeight>>> Getusers()
-//    {
-//        if (_context.users == null)
-//        {
-//            return NotFound();
-//        }
-//        return await _context.users.ToListAsync();
+        public UserWeightController(DataContext context)
+        {
+            _context = context;
+        }
 
-//    }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<object>>> Getusers()
+        {
+            var userWeights = await _context.UserWeights
+                .Select(uw => new { Weight = uw.Weight, Date = uw.date })
+                .ToListAsync();
 
-    
+            if (userWeights == null)
+            {
+                return NotFound();
+            }
+
+            return userWeights;
+        }
+
+    }
+}
