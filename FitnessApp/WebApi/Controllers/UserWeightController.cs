1994bb97f41c249c28b5a59edb1823830e6bfd21
Bateminho <93334175+Bateminho@users.noreply.github.com>
2023-05-04 17:47:19 +0200

    Updated the UserWeightController

--- FitnessApp/WebApi/Controllers/UserWeightController.cs
+++ FitnessApp/WebApi/Controllers/UserWeightController.cs
@@ -4,6 +4,7 @@ using FA_DB.Data;
 using FA_DB.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebApi.DTO;
 
 namespace WebApi.Controllers
 {
@@ -19,18 +20,121 @@ namespace WebApi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<object>>> Getusers()
+        public async Task<ActionResult<IEnumerable<object>>> GetUsers()
         {
             var userWeights = await _context.UserWeights
                 .Select(uw => new { Weight = uw.Weight, Date = uw.date })
                 .ToListAsync();
 
-            if (userWeights == null)
+            return userWeights;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserWeightDto>> GetUserWeight(int id)
+        {
+            var userWeight = await _context.UserWeights.FindAsync(id);
+
+            if (userWeight == null)
             {
                 return NotFound();
             }
 
-            return userWeights;
+            var userWeightDto = new UserWeightDto
+            {
+                Weight = userWeight.Weight,
+                Date = userWeight.date
+            };
+
+            return userWeightDto;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<UserWeightDto>> CreateUserWeight(UserWeightDto userWeightDto)
+        {
+            var userDataEmail = User.Identity.Name;
+            var userData = await _context.userDatas.SingleOrDefaultAsync(u => u.Email == userDataEmail);
+
+            var newUserWeight = new UserWeight
+            {
+                Weight = userWeightDto.Weight,
+                date = userWeightDto.Date,
+                UserData = userData
+            };
+
+            _context.UserWeights.Add(newUserWeight);
+            await _context.SaveChangesAsync();
+
+            var createdUserWeightDto = new UserWeightDto
+            {
+                Weight = newUserWeight.Weight,
+                Date = newUserWeight.date
+            };
+
+            return CreatedAtAction(nameof(GetUserWeight), new { id = newUserWeight.ID }, createdUserWeightDto);
+        }
+
+
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUserWeight(int id, UserWeightDto userWeightDto)
+        {
+            var userWeightToUpdate = await _context.UserWeights.FindAsync(id);
+
+            if (userWeightToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            userWeightToUpdate.Weight = userWeightDto.Weight;
+            userWeightToUpdate.date = userWeightDto.Date;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserWeightExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool UserWeightExists(int id)
+        {
+            return _context.UserWeights.Any(uw => uw.ID == id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserWeightDto>> DeleteUserWeight(int id)
+        {
+            var userWeightToDelete = await _context.UserWeights.FindAsync(id);
+
+            if (userWeightToDelete == null)
+            {
+                return NotFound();
+            }
+
+            var deletedUserWeight = new UserWeightDto
+            {
+                Weight = userWeightToDelete.Weight,
+                Date = userWeightToDelete.date
+            };
+
+            _context.UserWeights.Remove(userWeightToDelete);
+            await _context.SaveChangesAsync();
+
+            return deletedUserWeight;
         }
 
     }
