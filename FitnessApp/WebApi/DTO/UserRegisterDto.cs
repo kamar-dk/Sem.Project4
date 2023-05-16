f018ba439a830734a545e88850b89a6424430723
au682545 <tigriss.tiger@gmail.com>
2023-05-16 15:17:27 +0200

    Update UserRegisterDto.cs

--- FitnessApp/WebApi/DTO/UserRegisterDto.cs
+++ FitnessApp/WebApi/DTO/UserRegisterDto.cs
@@ -2,9 +2,9 @@
 {
     public class UserRegisterDto
     {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; }// = string.Empty;
+        public string Password { get; set; }// = string.Empty;
+        public string FirstName { get; set; }// = string.Empty;
+        public string LastName { get; set; }// = string.Empty;
     }
 }
