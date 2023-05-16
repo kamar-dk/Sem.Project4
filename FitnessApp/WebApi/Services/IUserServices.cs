851c6081aa7ab3154c65eb0cf0c5cf164c61bbda
au682545 <tigriss.tiger@gmail.com>
2023-05-16 15:17:29 +0200

    Create IUserServices.cs

---
+++ FitnessApp/WebApi/Services/IUserServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Mail;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using WebApi.Models;
+
+namespace WebApi.Services
+{
+    public interface IUserServices
+    {
+        public string CreateToken(User user);
+
+        public bool IsVaildEmail(string email);
+        
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        public bool TryVerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+    }
+}
+
