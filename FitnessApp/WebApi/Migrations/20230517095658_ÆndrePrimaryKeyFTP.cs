bd7237f335ff18d4a6a2b4eb0a44622654e0ff2e
au682545 <tigriss.tiger@gmail.com>
2023-05-17 12:16:14 +0200

    Create 20230517095658_ÆndrePrimaryKeyFTP.cs

---
+++ FitnessApp/WebApi/Migrations/20230517095658_ÆndrePrimaryKeyFTP.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ÆndrePrimaryKeyFTP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_favoriteTraningPrograms",
+                table: "favoriteTraningPrograms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_favoriteTraningPrograms_TraningProgramID",
+                table: "favoriteTraningPrograms");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_favoriteTraningPrograms",
+                table: "favoriteTraningPrograms",
+                column: "TraningProgramID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_favoriteTraningPrograms_Email",
+                table: "favoriteTraningPrograms",
+                column: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_favoriteTraningPrograms",
+                table: "favoriteTraningPrograms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_favoriteTraningPrograms_Email",
+                table: "favoriteTraningPrograms");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_favoriteTraningPrograms",
+                table: "favoriteTraningPrograms",
+                column: "Email");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_favoriteTraningPrograms_TraningProgramID",
+                table: "favoriteTraningPrograms",
+                column: "TraningProgramID");
+        }
+    }
+}
