d54ac9492d905417b1419cbcb03d17cf9ac76ed4
Hashem Abdou <98241106+MHA963@users.noreply.github.com>
2023-05-18 16:13:27 +0200

    trainingsprograms cards

--- FitnessApp/Fend/FitnessApp/src/Components/TrainingsPrograms.js
+++ FitnessApp/Fend/FitnessApp/src/Components/TrainingsPrograms.js
@@ -1,11 +1,13 @@
 import { React, useState } from 'react';
 import { useEffect } from 'react';
 import Dropdown from './Dropdown';
-import { Grid, Button, Typography, Card, CardContent } from "@material-ui/core";
+import { Grid, Button, Typography, Card, CardContent  , CardActions, IconButton} from "@material-ui/core";
+import { Favorite, FavoriteBorder } from "@material-ui/icons";
 
 function TrainingProgram() {
   const [data, setData] = useState([]);
   const [id, setId] = useState([])
+  const [selectedPrograms, setSelectedPrograms] = useState([]);
 
   const fetchData = () => {
     var url = "https://localhost:7221/api/TraningPrograms";
@@ -20,7 +22,15 @@ function TrainingProgram() {
       .then((response) => response.json())
       .then((data) => setData(data));
   }
-
+  const handleToggleFavorite = (programId) => {
+    setSelectedPrograms((prevSelectedPrograms) => {
+      if (prevSelectedPrograms.includes(programId)) {
+        return prevSelectedPrograms.filter((id) => id !== programId);
+      } else {
+        return [...prevSelectedPrograms, programId];
+      }
+    });
+  }
 
   // useEffect(() => {
   //     fetchData();
@@ -39,24 +49,31 @@ function TrainingProgram() {
         
       </Grid>
       <Grid container spacing={3}>
-        {data.map(item => (
-          <Grid item xs={12} sm={6} md={4} key={item.trainingProgramID}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  {item.name}
-                </Typography>
-                {/* Add more content or details about the training program */}
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+          {data.map(item => (
+            <Grid item xs={12} sm={6} md={4} key={item.trainingProgramID}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {item.name}
+                  </Typography>
+                  {/* Add more content or details about the training program */}
+                </CardContent>
+                <CardActions>
+                  <IconButton
+                    onClick={() => handleToggleFavorite(item.trainingProgramID)}
+                    color={selectedPrograms.includes(item.trainingProgramID) ? "secondary" : "default"}
+                  >
+                    {selectedPrograms.includes(item.trainingProgramID) ? <Favorite /> : <FavoriteBorder />}
+                  </IconButton>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
 
     </div>
 
   );
 }
-
 
 export default TrainingProgram;
