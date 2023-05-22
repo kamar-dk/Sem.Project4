ce0f8750260e2920105da7a7a31c6761f08b9c6f
au682545 <tigriss.tiger@gmail.com>
2023-05-22 10:49:50 +0200

    Update DataContext.cs

--- FitnessApp/WebApi/Data/DataContext.cs
+++ FitnessApp/WebApi/Data/DataContext.cs
@@ -37,33 +37,9 @@ namespace WebApi.Data
 
             base.OnModelCreating(modelBuilder);
 
-            // Define keys
+            // Define User
             modelBuilder.Entity<User>()
                 .HasKey(u => u.Email);
-            modelBuilder.Entity<UserData>()
-                .HasKey(ud => ud.Email);
-            modelBuilder.Entity<TraningData>()
-                .HasKey(td => td.Id);
-            modelBuilder.Entity<FavoriteTraningPrograms>()
-                .HasKey(ftp => ftp.FavoriteTraningProgramsID);
-            modelBuilder.Entity<FavoriteTraningPrograms>()
-                .HasIndex(ftp => ftp.FavoriteTraningProgramsID)
-                .IsUnique();
-            modelBuilder.Entity<TraningPrograms>()
-                .HasKey(tp => tp.TraningProgramID);
-            //modelBuilder.Entity<RunningSession>()
-            //    .HasKey(rs => rs.SessionID);
-            //modelBuilder.Entity<BikeSession>()
-            //    .HasKey(bs => bs.SessionID);
-            modelBuilder.Entity<UserWeight>()
-                .HasKey(uw => uw.ID);
-
-
-            // Define User Relationships
-            //modelBuilder.Entity<User>()
-            //    .HasOne(u => u.TraningDatas);                
-            //.WithOne(td => td.User)
-            //.HasForeignKey<TraningData>(td => td.Email);
             modelBuilder.Entity<User>()
                 .HasOne(u => u.UserData)
                 .WithOne(ud => ud.User)
@@ -73,38 +49,41 @@ namespace WebApi.Data
                 .WithOne(ftp => ftp.User)
                 .HasForeignKey(ftp => ftp.Email);
 
-            //modelBuilder.Entity<User>()
-            //    .HasOne(u => u.TraningDatas);
-            //.WithOne(td => td.User)
-            //.HasForeignKey<TraningData>(td => td.Email);
-
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.UserData)
-                .WithOne(ud => ud.User)
-                .HasForeignKey<UserData>(ud => ud.Email);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.FavoriteTraningPrograms)
-                .WithOne(ft => ft.User)
-                .HasForeignKey(ft => ft.Email);
-
-            modelBuilder.Entity<UserWeight>()
-                .HasOne(w => w.UserData)
-                .WithMany(uw => uw.UserWeights);
-
+            //Define UserData
+            modelBuilder.Entity<UserData>()
+                .HasKey(ud => ud.Email);
             modelBuilder.Entity<UserData>()
                 .HasMany(uw => uw.UserWeights);
 
+            // Define TraningData
+            modelBuilder.Entity<TraningData>()
+                .HasKey(td => td.Id);
 
+            // Define FavoriteTraningPrograms
+            modelBuilder.Entity<FavoriteTraningPrograms>()
+                .HasKey(ftp => ftp.FavoriteTraningProgramsID);
+            modelBuilder.Entity<FavoriteTraningPrograms>()
+                .HasIndex(ftp => ftp.FavoriteTraningProgramsID)
+                .IsUnique();
+
+            // Define TraningPrograms
+            modelBuilder.Entity<TraningPrograms>()
+                .HasKey(tp => tp.TraningProgramID);
             modelBuilder.Entity<FavoriteTraningPrograms>()
                 .HasOne(ftp => ftp.TraningProgram)
                 .WithMany(tp => tp.FavoriteTraningPrograms)
                 .HasForeignKey(ftp => ftp.TraningProgramID);
-
             modelBuilder.Entity<FavoriteTraningPrograms>()
                 .HasOne(u => u.User)
                 .WithMany(ftp => ftp.FavoriteTraningPrograms)
                 .HasForeignKey(ftp => ftp.Email);
+
+            // Define UserWeight
+            modelBuilder.Entity<UserWeight>()
+                .HasKey(uw => uw.ID);
+            modelBuilder.Entity<UserWeight>()
+                .HasOne(w => w.UserData)
+                .WithMany(uw => uw.UserWeights);
 
             //modelBuilder.Entity<FavoriteTraningPrograms>()
             //    .HasOne(tp => tp.TraningProgram)
