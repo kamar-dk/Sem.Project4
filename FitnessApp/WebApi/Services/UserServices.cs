981f9871159f6783f1c173d36774affa973a0ac8
AlanKhamo <alankhamo@gmail.com>
2023-05-24 12:51:31 +0200

    små ændringer

--- FitnessApp/WebApi/Services/UserServices.cs
+++ FitnessApp/WebApi/Services/UserServices.cs
@@ -54,17 +54,20 @@ namespace WebApi.Services
         }
         public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using var hmac = new HMACSHA512();
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
 
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
         public bool TryVerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
             using var hmac = new HMACSHA512(passwordSalt);
             var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            var acout = computedHash.SequenceEqual(passwordHash);
-            return computedHash.SequenceEqual(passwordHash);
+            bool isPasswordValid = computedHash.SequenceEqual(passwordHash);
+            return isPasswordValid;
         }
+
     }
 }
